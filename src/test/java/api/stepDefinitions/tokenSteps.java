9810e6c9504801c5ef79cad58c6f7acf78dfcf35
Your Name <you@example.com>
2025-03-08 14:43:14 -0500

    Update Token step definition

--- src/test/java/api/stepDefinitions/tokenSteps.java
+++ src/test/java/api/stepDefinitions/tokenSteps.java
@@ -15,30 +15,26 @@ public class tokenSteps {
     private static String token;
     static Response response;
     protected static final Logger logger = LogManager.getLogger(tokenSteps.class);
+
     @Given("set request for token generation")
     public void setRequestForTokenGeneration() {
 
-            JsonObject bodyJ = new JsonObject();
+        JsonObject bodyJ = new JsonObject();
         bodyJ.add("clientEmail", "tesyfdgookjko@gmail.com");
-            bodyJ.add("clientName", "Bawdtytwww");
-            String URL = "https://simple-books-api.glitch.me/api-clients/";
+        bodyJ.add("clientName", "Bawdtytwww");
+        String URL = "https://simple-books-api.glitch.me/api-clients/";
 
-            File eeee= new File("src/test/java/api/Payloads/tokenBody.json");
-            response = RestAssured.given()
-                    .body(eeee)
-                    .post(URL);
+        File eeee = new File("src/test/java/api/Payloads/tokenBody.json");
+        response = RestAssured.given()
+                .body(eeee)
+                .post(URL);
 
-        logger.info("bodyJ: "+bodyJ);
-        logger.info("response: "+response.asPrettyString());
+        logger.info("bodyJ: " + bodyJ);
+        logger.info("response: " + response.asPrettyString());
 
-            Assert.assertEquals(201,response.statusCode());
-            token = response.jsonPath().getString("accessToken");
-        }
-
-
-
-
-
+        Assert.assertEquals(201, response.statusCode());
+        token = response.jsonPath().getString("accessToken");
+    }
 
 
 }
